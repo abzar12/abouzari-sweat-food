097ae86627026b117e4bd132ef98f5d510836f58
AbouCloud <Abouzari6839@gmail.com>
2025-07-21 23:02:52 +0000

    footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 // end of slide import
 import Navbar from "./component/navbar";
+import Footer from "./component/footer";
 import { FaUtensils } from "react-icons/fa";
 import { FaUserTie } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
@@ -12,36 +13,37 @@ import { GiMeal } from "react-icons/gi";
 import './style/home.css'
 
 function Home() {
-     const [settings, setSettings] = useState({
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear"
-  });
-  useEffect(()=>{
-    setInterval(() => {
-        let Width = window.innerWidth;
-         if(Width >= "860"){
-        setSettings((prev) => {
-            return {...prev, slidesToShow: 3}
-        }) }
-        if(Width <= "860"){
-        setSettings((prev) => {
-            return {...prev, slidesToShow: 2}
-        })
-    }
-     if(Width <= "620"){
-        setSettings((prev) =>{
-            return {...prev, slidesToShow: 1}
-        })
-    }
-    }, 1000);
-  },[])
-  
+    const [settings, setSettings] = useState({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+    });
+    useEffect(() => {
+        setInterval(() => {
+            let Width = window.innerWidth;
+            if (Width >= "860") {
+                setSettings((prev) => {
+                    return { ...prev, slidesToShow: 3 }
+                })
+            }
+            if (Width <= "860") {
+                setSettings((prev) => {
+                    return { ...prev, slidesToShow: 2 }
+                })
+            }
+            if (Width <= "620") {
+                setSettings((prev) => {
+                    return { ...prev, slidesToShow: 1 }
+                })
+            }
+        }, 1000);
+    }, [])
+
 
     const card = [
         { id: 1, name: "Burger", url: "./rest-img/burger.jpg", price: "200 $" },
@@ -52,22 +54,22 @@ function Home() {
         { id: 6, name: "Beef Burgers", url: "./rest-img/burge2.jpg", price: "200 $" }
     ]
     const Chefs = [
-    {id: 1, img:"./rest-img/Chef1.jpg",  alt:"chef1",  name:"Mohamed", experience: "20 ans"},
-     {id: 2, img:"./rest-img/Chef2.jpg", alt:"chef2",  name:"Alassane", experience: "01 ans"},
-     {id: 3, img:"./rest-img/Chef3.jpg", alt:"chef3",  name:"Fanta", experience: "10 ans"},
-     {id: 4, img:"./rest-img/Chef4.jpg", alt:"chef4",  name:"Oumar", experience: "05 ans"},
-     {id: 5, img:"./rest-img/Chef5.jpg", alt:"chef5",  name:"Hamath", experience: "02 ans"},
-     {id: 6, img:"./rest-img/Chef6.jpg", alt:"chef6",  name:"Joe", experience: "15 ans"},
-     {id: 7, img:"./rest-img/Chef7.jpg", alt:"chef7",  name:"Kalidy", experience: "07 ans"},
-     {id: 8, img:"./rest-img/Chef8.jpg", alt:"chef7",  name:"Moussa", experience: "20 ans"}
-  ]
-  const testimonial =[
-    {id: 1, message:"je suis née en mille neuf cent quatre ving je suis gentille and j'aime travailler", url:"./rest-img/Chef1.jpg", name: "Nama"},
-    {id: 2, message:"je suis née en mille neuf cent quatre ving je suis gentille and j'aime travailler", url:"./rest-img/Chef1.jpg", name: "Nama"},
-    {id: 3, message:"je suis née en mille neuf cent quatre ving je suis gentille and j'aime travailler", url:"./rest-img/Chef1.jpg", name: "Nama"},
-    {id: 4, message:"je suis née en mille neuf cent quatre ving je suis gentille and j'aime travailler", url:"./rest-img/Chef1.jpg", name: "Nama"},
-  ]
-  
+        { id: 1, img: "./rest-img/Chef1.jpg", alt: "chef1", name: "Mohamed", experience: "20 ans" },
+        { id: 2, img: "./rest-img/Chef2.jpg", alt: "chef2", name: "Alassane", experience: "01 ans" },
+        { id: 3, img: "./rest-img/Chef3.jpg", alt: "chef3", name: "Fanta", experience: "10 ans" },
+        { id: 4, img: "./rest-img/Chef4.jpg", alt: "chef4", name: "Oumar", experience: "05 ans" },
+        { id: 5, img: "./rest-img/Chef5.jpg", alt: "chef5", name: "Hamath", experience: "02 ans" },
+        { id: 6, img: "./rest-img/Chef6.jpg", alt: "chef6", name: "Joe", experience: "15 ans" },
+        { id: 7, img: "./rest-img/Chef7.jpg", alt: "chef7", name: "Kalidy", experience: "07 ans" },
+        { id: 8, img: "./rest-img/Chef8.jpg", alt: "chef7", name: "Moussa", experience: "20 ans" }
+    ]
+    const testimonial = [
+        { id: 1, message: "One of the best dining experiences I’ve had in a while. The chefs deserve all the praise for their creativity and skill.", url: "./rest-img/Chef1.jpg", name: "Abouzari" },
+        { id: 2, message: "One of the best dining experiences I’ve had in a while. The chefs deserve all the praise for their creativity and skill.", url: "./rest-img/Chef1.jpg", name: "Abouzari" },
+        { id: 3, message: "One of the best dining experiences I’ve had in a while. The chefs deserve all the praise for their creativity and skill.", url: "./rest-img/Chef1.jpg", name: "Abouzari" },
+        { id: 4, message: "One of the best dining experiences I’ve had in a while. The chefs deserve all the praise for their creativity and skill.", url: "./rest-img/Chef1.jpg", name: "Abouzari" },
+    ]
+
     return (
         <>
             <header>
@@ -87,7 +89,7 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section className="our-menu">
+                <section className="our-menu" id="menu">
                     <div className="ac-menu">
                         <h2>Our<span>-</span>Menu</h2>
                         <div className="ac-card">
@@ -135,7 +137,7 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section className="AboutUs">
+                <section className="AboutUs" id="about">
                     <div className="ac-about">
                         <h2>About <span>Us</span></h2>
                         <div className="ac-content">
@@ -157,41 +159,43 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                
-                <section className="teams">
+
+                <section className="teams" id="teams">
                     <div className="ac-teams">
                         <h1>Our Teams</h1>
                         <h2>Our Teams</h2>
                         <div className="teams slider-container">
                             <Slider {...settings}>
-                            {
-                                Chefs.map((chef, index) => (
-                                    <div className="team" key={index}>
-                                        <div className="img"> <img src={chef.img} alt={chef.alt} width={5}/>
-                                        <h3>{chef.name}</h3>
+                                {
+                                    Chefs.map((chef, index) => (
+                                        <div className="team" key={index}>
+                                            <div className="img">
+                                                <img src={chef.img} alt={chef.alt} width={5} />
+                                            </div>
+                                            <h3>{chef.name}</h3>
+                                            <p>{chef.experience} Experiences</p>
                                         </div>
-                                        
-                                        <p>{chef.experience} Experiences</p>
-                                    </div>
-                                ))
-                            }
+                                    ))
+                                }
                             </Slider>
                         </div>
                     </div>
                 </section>
                 <section className="testimonial">
                     <div className="ac-testimonial">
-                        <h1>Testimonial</h1>
+                        <h1>Testimonials</h1>
                         <h2>Ours Clients Said</h2>
                         <div className="ac-messages">
                             {
-                                testimonial.map((testi) =>(
+                                testimonial.map((testi) => (
                                     <div className="contente" key={testi.key}>
-                                        <div className="">
+                                        <div className="image">
                                             <img src={testi.url} alt="" />
-                                            <h3> {testi.name} </h3>
                                         </div>
-                                        <p>{testi.message} </p>
+                                        <div className="">
+                                            <h3> {testi.name} </h3>
+                                            <p>{testi.message} </p>
+                                        </div>
                                     </div>
                                 ))
                             }
@@ -199,6 +203,9 @@ function Home() {
                     </div>
                 </section>
             </main>
+            <footer>
+                <Footer />
+            </footer>
         </>
     )
 }

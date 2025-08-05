c13f29f1a4d699fdabe65f32d097ed59016979c2
AbouCloud <Abouzari6839@gmail.com>
2025-08-05 21:33:06 +0000

    restaurant final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/End-of-day-Challenges/',
+  base: '/abouzari-sweat-food/',
 })

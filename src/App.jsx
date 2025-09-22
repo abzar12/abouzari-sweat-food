bb97fde5065fb36841bf5ed1ca727ee77dccc9b4
AbouCloud <Abouzari6839@gmail.com>
2025-09-22 18:57:16 +0000

    responsible added to restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 //import { Router } from "react-router-dom"
 import Home from "./Home";
+import { Router, Route } from "react-router-dom";
 function App(){
   return (
     <>
+    
     <Home />
     </>
   )
